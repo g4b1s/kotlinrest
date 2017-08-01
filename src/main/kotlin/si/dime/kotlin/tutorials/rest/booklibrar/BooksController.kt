97d60b0f31fed219bbb26889c95da2e40ffa2cf2
Gabriel <gabriel@localhost.localdomain>
2017-08-01 17:42:13 -0400

    exceptions

--- src/main/kotlin/si/dime/kotlin/tutorials/rest/booklibrar/BooksController.kt
+++ src/main/kotlin/si/dime/kotlin/tutorials/rest/booklibrar/BooksController.kt
@@ -18,8 +18,20 @@ class BooksController{
             else throw DuplicateItemException()
 
     @RequestMapping("/isbn",  method = arrayOf(RequestMethod.GET))
-    fun getisbn(@RequestBody  book: Book) = database.getisbn(book.ISBN)
+    fun getisbn(@RequestBody  book: Book): Book? {
+        database.getisbn(book.ISBN).let {
+            if (it == null) throw NotFoundItemException()
+            return it
+        }
+    }
 
     @RequestMapping("/iisbn/{isbnCode}",  method = arrayOf(RequestMethod.GET))
-    fun getisbn(@PathVariable isbnCode: String) = database.getisbn(isbnCode)
+    fun getisbn(@PathVariable isbnCode: String): Book? {
+        database.getisbn(isbnCode).let {
+            if (it == null) throw NotFoundItemException()
+            return it
+        }
+
+    }
+
 }
